f447e5260e92a59b8ed8830c94eff6f1a7f1b3e6
Daniel Roman <dan.roman21@gmail.com>
2020-05-22 09:18:47 -0400

    Updated Pet rev3

--- VirtualPet/Pet.cs
+++ VirtualPet/Pet.cs
@@ -10,13 +10,27 @@ namespace VirtualPet
 
 
         public string Species { get; set; }
-
         public string Name { get; set; }
+        public int Boredom { get; set; }
+        public int Hunger { get; set; }
+        public int Health { get; set; }
+
+
         public Pet(string name, string species)
         {
             Name = name;
             Species = species;
+            Hunger = 30;
+            Boredom = 25;
+            Health = 47;
         }
+        public Pet()
+        {
+            Hunger = 30;
+            Boredom = 25;
+            Health = 47;
+        }
+
         public void SetSpecies(string species)
         {
             Species = species;
@@ -24,22 +38,22 @@ namespace VirtualPet
         public void SetName(string name)
         {
             Name = name;
-         
+
         }
-        public string GetName(string name)
+        public string GetName()
         {
-            return name;
+            return Name;
+
+        }
+        public string GetSpecies()
+        {
+            return Species;
+        }
+        public void Feed()
+        {
+            Hunger = Hunger - 15;
+        }
     }
-        }
-        public string GetSpecies(string name)
-        {
-            return species;
-        }
-        public double Hunger()
-        {
-        return Hunger;
-        }
-        public double Boredom { get; set; }
     
 
 }
