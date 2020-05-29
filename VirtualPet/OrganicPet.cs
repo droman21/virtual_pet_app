95af048d2be91a4c4db2292e0965f3b39676a145
Daniel Roman <dan.roman21@gmail.com>
2020-05-29 14:38:27 -0400

    Updated Shelter rev24

---
+++ VirtualPet/OrganicPet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualPet
+{
+    class OrganicPet : Pet
+    {
+
+    }
+}
--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -18,12 +18,13 @@ namespace VirtualPet
             {
                 Console.WriteLine("Hi and Welcome to the Perrysburg Pet Shelter!");
                 Console.WriteLine("What would you like to do?");
-                Console.WriteLine("1. I'm Bringing in a new Pet");
-                Console.WriteLine("2. Feed all of the Pets");
-                Console.WriteLine("3. Play with all of the Pets");
-                Console.WriteLine("4. Play with a single Pet");
-                Console.WriteLine("5. Adopt a Pet");
-                Console.WriteLine("6. Quit");
+                Console.WriteLine("1. I'm Bringing in a New Organic Pet");
+                Console.WriteLine("2. I'm Brining in a New Mechcanical Pet");
+                Console.WriteLine("3. Feed all of the Pets");
+                Console.WriteLine("4. Play with all of the Pets");
+                Console.WriteLine("5. Play with a Single Pet");
+                Console.WriteLine("6. Adopt a Pet");
+                Console.WriteLine("7. Leave the Shelter");
                 //Console.Clear();
                 string menuChoice = Console.ReadLine().ToLower();
 
@@ -31,31 +32,42 @@ namespace VirtualPet
                 {
                     case "1":
                         newPet = new Pet();
-                        Console.WriteLine("What is your pet's name?");
+                        Console.WriteLine("What is your organic pet's name?");
                         string name = Console.ReadLine();
                         newPet.SetName(name);
 
-                        Console.WriteLine("What species is your pet?");
+                        Console.WriteLine("What species is your organic pet? (Example: tiger, dog, fish");
                         string species = Console.ReadLine();
                         newPet.SetSpecies(species);
                         petShelter.AddPet(newPet);
                         petShelter.PrintAllPets();
                         Console.WriteLine("\n");
-
                         break;
-                    case "2":
+                    //case "2":
+                        Console.WriteLine("What is your mechcanical pet's name?");
+                        string name = Console.ReadLine();
+                        newPet.SetName(name);
+
+                        Console.WriteLine("What type is your pet? (Example: Robot or Cyborg");
+                        string species = Console.ReadLine();
+                        newPet.SetSpecies(species);
+                        petShelter.AddPet(newPet);
+                        petShelter.PrintAllPets();
+                        Console.WriteLine("\n");
+                        break;
+                    case "3":
                         petShelter.PrintAllPets();
                         petShelter.FeedAll();
                         Console.WriteLine("You fed the pets!");
                         Console.WriteLine("\n");
                         break;
-                    case "3":
+                    case "4":
                         petShelter.PrintAllPets();
                         petShelter.PlayAll();
                         Console.WriteLine("You played with the pets!");
                         Console.WriteLine("\n");
                         break;
-                    case "4":
+                    case "5":
                         petShelter.PrintAllPets();
                         Console.WriteLine("Which pet would you like to play with?");
                         string petToPlay = Console.ReadLine();
@@ -64,7 +76,7 @@ namespace VirtualPet
                         Console.WriteLine($"You played with {newPet.GetName()}!");
                         Console.WriteLine("\n");
                         break;
-                    case "5":
+                    case "6":
                         petShelter.PrintAllPets();
                         Console.WriteLine("Which pet would you like to adopt?");
                         string petToAdopt = Console.ReadLine();
@@ -73,7 +85,7 @@ namespace VirtualPet
                         Console.WriteLine($"You gave {newPet.GetName()} a good home!");
                         Console.WriteLine("\n");
                         break;
-                    case "6":
+                    case "7":
                         keepThinking = false;
                         Console.WriteLine("Good Bye! Thanks for Visiting!");
                         break;
