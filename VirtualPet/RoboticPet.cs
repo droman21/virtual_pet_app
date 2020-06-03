82c63063fd1188a365e5ef10da5f3f7161f00512
Daniel Roman <dan.roman21@gmail.com>
2020-06-03 05:41:44 -0400

    Updated Shelter with Robots rev2

--- VirtualPet/RoboticPet.cs
+++ VirtualPet/RoboticPet.cs
@@ -13,5 +13,75 @@ namespace VirtualPet
         //Give Oil to Robotic Pets
 
         //Take Robotic Pet For Maintenance
+
+        public string RobotPetType { get; set; }
+        public string RobotPetName { get; set; }
+        public int GiveRobotOil { get; set; }
+        
+
+        public RoboticPet(string robotname, string type)
+        {
+            Name = robotname;
+            PetType = type;
+            Boredom = 60;
+            OilLevel = 60;
+            Performance = 30;
+        }
+        public RoboticPet()
+        {
+            Boredom = 60;
+            OilLevel = 60;
+            Performance = 30;
+            MaintenanceLevel = 45;
+        }
+        public void SetRobotName(string robotname)
+        {
+            Name = robotname;
+        }
+        public void SetRobotType(string type)
+        {
+            PetType = type;
+        }
+        public string GetRobotType()
+        {
+            return PetType;
+        }
+        public string GetRobotName()
+        {
+            return Name;
+        }
+        public int GetMaintenance()
+        {
+            return MaintenanceLevel;
+        }
+        public int GetOilLevel()
+        {
+            return OilLevel;
+        }
+        public void Feed()
+        {
+            Hunger -= 40;
+        }
+        public void SeeDoctor()
+        {
+            Health += 30;
+        }
+        public void Play()
+        {
+            Hunger += 10;
+            Boredom -= 20;
+            Health += 10;
+        }
+        public void Tick()
+        {
+            Hunger += 5;
+            Boredom += 5;
+            Health -= 5;
+        }
+
+    }
+
+
+}
     }
 }
